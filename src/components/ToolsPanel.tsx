f97e0c8589bf9c02d2f661b6f243d91144c4b9bf
MichalZard <miso.platko@gmail.com>
2023-05-01 23:20:19 +0200

    cmc icon update

--- src/components/ToolsPanel.tsx
+++ src/components/ToolsPanel.tsx
@@ -15,9 +15,9 @@ function ToolsPanel({ title, tools }: ToolsPanelProps) {
                 {title}
             </section >
             <section className="w-full">
-                {tools.map(tool => (
-                    <a href={tool.link} target="_blank" className="flex p-8 border border-kryptokurz-gray border-t-0">
-                        <img className="w-12 h-12 mr-5" src={tool.img} />
+                {tools.map((tool, i) => (
+                    <a key={i} href={tool.link} target="_blank" className="flex p-8 border border-kryptokurz-gray border-t-0">
+                        <img className="w-12 h-12 mr-5 object-cover" src={tool.img} />
                         <section className="flex flex-col">
                             <span className="text-white text-lg font-semibold">{tool.name}</span>
                             <span className="text-kryptokurz-gray text-md font-semibold pl-0 p-1">"{tool.description}"</span>
