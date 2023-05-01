a3e10bbbd8ac165eaf27ed4c04f17bfc98fd7758
MichalZard <miso.platko@gmail.com>
2023-05-02 01:04:48 +0200

    sharper video,mobile version cookie banner

--- src/components/CookieBanner.tsx
+++ src/components/CookieBanner.tsx
@@ -12,15 +12,17 @@ function CookieBanner() {
         }
     }, []);
     return (
-        <section className={`${open ? "visible" : "hidden"} w-full h-14 bg-kryptokurz-cookie-banner text-xs text-white p-4 flex items-center justify-between`}>
+        <section className={`${open ? "visible" : "hidden"} w-full min-h-14 bg-kryptokurz-cookie-banner text-xs whitespace-pre-line text-white p-4 flex flex-wrap lg:flex-nowrap  items-center
+        justify-between
+        `}>
             <p>
                 Táto webová stránka používa súbory cookie, aby ste pri jej návšteve mali čo najlepší zážitok.
                 Pre viac informácií si prečítajte naše <a href="/gdpr" className="underline text-gray-400 ml-1">zásady ochrany osobných údajov</a>.
                 Ak si želáte akceptovať použitie nepodstatných súborov cookie, kliknite na tlačidlo "Súhlasím"
             </p>
-            <section className="mr-4 [&>button]:mx-1">
+            <section className="mr-4 [&>button]:mx-1 my-2 flex">
                 <button className="border border-white py-1.5 px-4" onClick={() => { setOpen(false); }}>Zamietnuť</button>
-                <button className="border border-white py-1.5 px-4" onClick={() => { setOpen(false); localStorage.setItem("cookie_ack", "true"); }}>Povoliť</button>
+                <button className="border border-white py-1.5 px-4" onClick={() => { setOpen(false); localStorage.setItem("cookie_ack", "true"); }}>Súhlasím</button>
             </section>
         </section>
     )
